e8d2552b894a01ba11a907098444bc1e9f779d5f
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-02-17 21:40:30 +0900

    refactor(a2dg): Migrate interfaces to Kotlin - IGameObject.java → IGameObject.kt   * fun update()   * fun draw(canvas: Canvas) - ITouchable.java → ITouchable.kt   * fun onTouchEvent(e: MotionEvent): Boolean - IRecyclable.java → IRecyclable.kt   * fun onRecycle() - IBoxCollidable.java → IBoxCollidable.kt   * fun getCollisionRect(): RectF - ILayerProvider.java → ILayerProvider.kt   * interface ILayerProvider<E : Enum<E>> : IGameObject   * fun getLayer(): E Improvements: - Null-safety with non-nullable return types - Kotlin interface syntax (fun instead of public void/return type) - Generic type parameter syntax: extends → : - Inheritance syntax: extends → : - Removed redundant 'public' keyword (implicit in Kotlin) All 5 interface files successfully migrated.

--- TuDefence/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/interfaces/ILayerProvider.java
+++
@@ -1,5 +0,0 @@
-package kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces;
-
-public interface ILayerProvider<E extends Enum<E>> extends IGameObject {
-    public E getLayer();
-}
--- TuDefence/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/interfaces/ITouchable.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces;
-
-import android.view.MotionEvent;
-
-public interface ITouchable {
-    public boolean onTouchEvent(MotionEvent e);
-}
